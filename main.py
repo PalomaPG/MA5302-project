a74d748c9598d4da085e067f36d8b106e827301f
PalomaPG <paloma.perez.gar@gmail.com>
2018-08-22 17:44:15 -0300

    clustering

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import sys, os
+import sys
 from src.Clustering import Clustering
 
 
 def main(path_):
     clust = Clustering(path_)
     clust.load_data()
-    clust.clusterization()
-
+    clust.plot_misclassify_count()
 
 if __name__ == '__main__':
     main(sys.argv[1])
